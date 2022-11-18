072ca388ddc4b4642e573b2dfe6bda648c15477f
anil pothuri <pothurianil.naidu@extwebtech.in>
2022-11-18 13:22:24 +0530

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,19 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-
-  
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test 'should get help' do
+    get help_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
-  test "should get about" do
-get static_pages_about_url
-assert_response :success
-end
-
+  test 'should get about' do
+    get about_url
+    assert_response :success
+  end
 end
